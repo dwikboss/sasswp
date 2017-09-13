746444b19a4638da5321874204cc91808a71056e
Dwiky <dwikboss99@gmail.com>
2017-09-13 11:19:03 +0200

    Bootstrap overrides Sass t2

--- functions.php
+++ functions.php
@@ -35,7 +35,7 @@ if ( ! function_exists( 'sasswp_setup' ) ) :
 		 */
 		add_theme_support( 'title-tag' );
 
-		/* Custom Logo Support */
+		// Add logo upload in customizer WordPress 4.5+
 		add_theme_support( 'custom-logo' );
 
 		/*
@@ -124,7 +124,7 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
- * Bootstrap Walker
+ * Bootstrap Walker Menu
  */
  require get_template_directory() . '/inc/bootstrap-walker.php';
 
@@ -136,24 +136,23 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 if ( !function_exists( 'sasswp_the_custom_logo' ) ) :
-
-/**
+	/**	
 	* Displays the optional custom logo.
 	*
 	* Does nothing if the custom logo is not available.
 	*
 	*/
-function sasswp_the_custom_logo() {
-	// Try to retrieve the Custom Logo
-	$output = '';
-	if (function_exists('get_custom_logo'))
-		$output = get_custom_logo();
-	
-	// Nothing in the output: Custom Logo is not supported, or there is no selected logo
-	// In both cases we display the site's name
-	if (empty($output))
-		$output = '<a class="navbar-brand" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
-	
-	echo $output;
-}
+	function sasswp_the_custom_logo() {
+		// Try to retrieve the Custom Logo
+		$output = '';
+		if (function_exists('get_custom_logo'))
+			$output = get_custom_logo();
+
+		// Nothing in the output: Custom Logo is not supported, or there is no selected logo
+		// In both cases we display the site's name
+		if (empty($output))
+			$output = '<a class="navbar-brand" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
+
+		echo $output;
+	}
 endif;
